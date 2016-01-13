5e530983118267448cfaa91a6fc11c0bc1615130
Thales Francisco <arquimago@users.noreply.github.com>
2016-01-13 07:59:15 -0300

    Corrigindo bug
    URLs com espaço não apareciam corretamente, substitui espaços por %20 e agora funciona

--- lolwikibot.py
+++ lolwikibot.py
@@ -14,14 +14,16 @@ def send_welcome(message):
 def query_text(inline_query):
 	try:
 		s = wikia.search(lol,inline_query.query)
-        url1 = wikia.page(lol,s[0]).url
-        url2 = wikia.page(lol,s[1]).url
+        	url1 = wikia.page(lol,s[0]).url
+        	url1 = url1.replace(' ', '%20')
+        	url2 = wikia.page(lol,s[1]).url
+        	url2 = url2.replace(' ', '%20')
 		title1 = wikia.page(lol,s[0]).title
 		title2 = wikia.page(lol,s[1]).title
-        r = types.InlineQueryResultArticle('1', title1, url1)
-        r2 = types.InlineQueryResultArticle('2', title2, url2)
-        bot.answer_inline_query(inline_query.id, [r, r2])
-    except Exception as e:
-        print(e)
+        	r = types.InlineQueryResultArticle('1', title1, url1)
+        	r2 = types.InlineQueryResultArticle('2', title2, url2)
+        	bot.answer_inline_query(inline_query.id, [r, r2])
+    	except Exception as e:
+        	print(e)
 
 bot.polling()
