01135c4b7ce7604ae2a7d4b6d70a7ad89c24e6a3
Francisco Cabral <franciscocabralfilho@gmail.com>
2016-02-25 19:16:50 +0000

    Mostra 10 resultados

--- lolwikibot.py
+++ lolwikibot.py
@@ -15,27 +15,20 @@ def send_welcome(message):
 @bot.inline_handler(lambda query: query.query)
 def query_text(inline_query):
 	try:
+		results = range(0,10)
 		s = wikia.search(lol,inline_query.query)
-		url1 = wikia.page(lol,s[0]).url
-		url1 = url1.replace(' ', '%20')
-		url2 = wikia.page(lol,s[1]).url
-		url2 = url2.replace(' ', '%20')
-		title1 = wikia.page(lol,s[0]).title
-		title2 = wikia.page(lol,s[1]).title
-		r = types.InlineQueryResultArticle('1', title1, url1)
-		r2 = types.InlineQueryResultArticle('2', title2, url2)
-		bot.answer_inline_query(inline_query.id, [r, r2])
-#		resultado=[]
-#		contador=0
-#		for results in s:
-#			print(result.title)
-#			url = wikia.page(lol,s[contador]).url
-#			url = url.replace(' ','%20')
-#			title = wikia.page(lol,s[contador]).url
-#			contador += 1
-#			resultado+=types.InlineQueryResultArticle(contador,title,url)
-#		bot.answer_inline_query(inline_query.id, resultado)
+		
+		for i in range(0,10):
+		
+			url = wikia.page(lol,s[i]).url
+			url = url.replace(' ', '%20')
+			title = wikia.page(lol,s[i]).title
+			id = "%d"%i
+			results[i] = types.InlineQueryResultArticle(id, title, url)
+		
+		bot.answer_inline_query(inline_query.id, results)
+		
 	except Exception as e:
-		print("Olha o erro...")
+		print(e)
 
 bot.polling()
