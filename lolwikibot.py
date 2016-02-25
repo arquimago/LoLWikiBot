e31a5054683f4ee7f44bed96b112e8c1c11a3f68
Thales Francisco <arquimago@users.noreply.github.com>
2016-02-25 20:32:09 -0300

    agora o bot dá até 30 resultados
    Podendo configurar pra N resultados

--- lolwikibot.py
+++ lolwikibot.py
@@ -14,19 +14,23 @@ def send_welcome(message):
 
 @bot.inline_handler(lambda query: query.query)
 def query_text(inline_query):
+	total_de_resultados = 30
+	erro_lol = "The Journal of Justice:"
 	try:
-		s = wikia.search(lol,inline_query.query,10)
-		results = range(0,len(s))
+		s = wikia.search(lol,inline_query.query,total_de_resultados)
+		resultados = range(0,len(s))
 		
 		for i in range(0,len(s)):
-		
+			erro = s[i].find(erro_lol)
+			if(erro>-1):
+				break
 			url = wikia.page(lol,s[i]).url
 			url = url.replace(' ', '%20')
 			title = wikia.page(lol,s[i]).title
 			id = "%d"%i
-			results[i] = types.InlineQueryResultArticle(id, title, url)
+			resultados[i] = types.InlineQueryResultArticle(id, title, url)
 		
-		bot.answer_inline_query(inline_query.id, results)
+		bot.answer_inline_query(inline_query.id, resultados)
 		
 	except Exception as e:
 		print(e)
