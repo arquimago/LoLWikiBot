3f5e6042da41c02b1a0022355e7cc2183719b01f
Francisco Cabral <franciscocabralfilho@gmail.com>
2016-02-26 01:01:05 +0000

    Configuration file

--- lolwikibot.py
+++ lolwikibot.py
@@ -2,10 +2,14 @@
 
 import wikia
 import telebot
+import ConfigParser
 from telebot import types
 
-TOKEN = "SEU TOKEN AQUI"
-lol = "leagueoflegends"
+config = ConfigParser.RawConfigParser()
+config.read('config.ini')
+
+TOKEN = config.get('bot father', 'token')
+wiki_name = config.get('wikia', 'wiki_name')
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -17,16 +21,16 @@ def query_text(inline_query):
 	total_de_resultados = 30
 	erro_lol = "The Journal of Justice:"
 	try:
-		s = wikia.search(lol,inline_query.query,total_de_resultados)
+		s = wikia.search(wiki_name,inline_query.query,total_de_resultados)
 		resultados = range(0,len(s))
 		
 		for i in range(0,len(s)):
 			erro = s[i].find(erro_lol)
 			if(erro>-1):
 				break
-			url = wikia.page(lol,s[i]).url
+			url = wikia.page(wiki_name,s[i]).url
 			url = url.replace(' ', '%20')
-			title = wikia.page(lol,s[i]).title
+			title = wikia.page(wiki_name,s[i]).title
 			id = "%d"%i
 			resultados[i] = types.InlineQueryResultArticle(id, title, url)
 		
