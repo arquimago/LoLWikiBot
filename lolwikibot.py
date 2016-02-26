84d9b256640f6346e16e55375c7cea26c73bf761
Thales Francisco <arquimago@users.noreply.github.com>
2016-02-25 21:53:43 -0300

    Ajustando comandos
    separei start de help e adicinoei info

--- lolwikibot.py
+++ lolwikibot.py
@@ -8,9 +8,17 @@ TOKEN = "SEU TOKEN AQUI"
 lol = "leagueoflegends"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Este bot faz buscas na Wiki de League of Legends, feito por @Arquimago")
+	bot.reply_to(message, "Este bot faz buscas inline na Wiki de League of Legends, feito por @Arquimago")
+	
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+	bot.reply_to(message, "Para utilizar esse bot apenas escreva @lolwbot e os termos da busca, não envie a mensagem apenas aguarde os resultados numa caixa popup")
+	
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+	bot.reply_to(message, "O propósito deste bot é apenas fazer buscas inline, se deseja mais interações e informações sobre o jogo, campeões, invocadores, partidas e tudo mais recomendo utilizar o @League_of_Legends_bot criado pelo @Edurolp e com tradução para Português feita por mim (@Arquimago).")
 
 @bot.inline_handler(lambda query: query.query)
 def query_text(inline_query):
