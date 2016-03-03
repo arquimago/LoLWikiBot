82dd7b47bb604de55895ad555b5dce4269dc811a
Thales Francisco <arquimago@users.noreply.github.com>
2016-03-02 22:05:00 -0300

    Update lolwikibot.py

--- lolwikibot.py
+++ lolwikibot.py
@@ -2,49 +2,53 @@
 
 import wikia
 import telebot
-import ConfigParser
 from telebot import types
+import requests
 
-config = ConfigParser.RawConfigParser()
-config.read('config.ini')
-
-TOKEN = config.get('bot father', 'token')
-wiki_name = config.get('wikia', 'wiki_name')
+TOKEN = "YOUR TOKEN HERE"
+lol = "leagueoflegends"
 bot = telebot.TeleBot(TOKEN)
 
+teclado = types.ReplyKeyboardMarkup(resize_keyboard=True)
+teclado.row('start')
+teclado.row('help')
+teclado.row('info')
+
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def send_welcome(message,reply_markup=teclado):
 	bot.reply_to(message, "Este bot faz buscas inline na Wiki de League of Legends, feito por @Arquimago")
 	
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
+def send_welcome(message,reply_markup=teclado):
 	bot.reply_to(message, "Para utilizar esse bot apenas escreva @lolwbot e os termos da busca, não envie a mensagem apenas aguarde os resultados numa caixa popup")
 	
 @bot.message_handler(commands=['info'])
-def send_welcome(message):
+def send_welcome(message,reply_markup=teclado):
 	bot.reply_to(message, "O propósito deste bot é apenas fazer buscas inline, se deseja mais interações e informações sobre o jogo, campeões, invocadores, partidas e tudo mais recomendo utilizar o @League_of_Legends_bot criado pelo @Edurolp e com tradução para Português feita por mim (@Arquimago).")
 
 @bot.inline_handler(lambda query: query.query)
 def query_text(inline_query):
-	total = 30
-	erro_wiki = "The Journal of Justice:"
+	total_de_resultados = 10
+	erro_lol = "The Journal of Justice:"
 	try:
-		search = wikia.search(wiki_name,inline_query.query,total)
-		results = range(0,len(s))
+		s = wikia.search(lol,inline_query.query,total_de_resultados)
+		resultados = range(0,len(s))
 		
 		for i in range(0,len(s)):
-			error = search[i].find(erro_wiki)
-			if(error>-1):
+			erro = s[i].find(erro_lol)
+			if(erro>-1):
 				break
-			url = wikia.page(wiki_name,search[i]).url
+			url = wikia.page(lol,s[i]).url
 			url = url.replace(' ', '%20')
-			title = wikia.page(wiki_name,search[i]).title
-			id = "%d"%(i+1)
-			results[i] = types.InlineQueryResultArticle(id, title, url)
+			title = wikia.page(lol,s[i]).title
+			id = "%d"%i
+			resultados[i] = types.InlineQueryResultArticle(id, title, url)
 		
-		bot.answer_inline_query(inline_query.id, results)
+		bot.answer_inline_query(inline_query.id, resultados)
 		
 	except Exception as e:
 		print(e)
+
+requests.packages.urllib3.disable_warnings()
 
 bot.polling(none_stop=True, interval=0, timeout=20)
